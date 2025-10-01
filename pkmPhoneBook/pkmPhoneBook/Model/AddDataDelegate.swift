25027c8dce4bd5f798ef4f2c7992bdea75322c2f
104hyeon <104hyeon@gmail.com>
2025-10-01 11:39:23 +0900

    [feature] #26 Lv8. 테이블뷰 셀 수정 후 저장 및 업데이트

--- pkmPhoneBook/pkmPhoneBook/Model/AddDataDelegate.swift
+++ pkmPhoneBook/pkmPhoneBook/Model/AddDataDelegate.swift
@@ -3,5 +3,8 @@ import UIKit
 import Foundation
 
 protocol AddDataDelegate: AnyObject {
+    // "추가"버튼 누를 때 사용
     func addContact(contact: Contact)
+    // 셀 누르고 수정할 때 사용
+    func editContact(contact: Contact, at index: Int)
     }
